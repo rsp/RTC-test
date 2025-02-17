4ea52267f0323be2a81734167cc711a7f38d9904
Rafał Pocztarski <r.pocztarski@gmail.com>
2025-02-17 09:53:50 +0100

    Add SimulationClient service

---
+++ src/simulationClient.ts
@@ -0,0 +1,20 @@
+import { Config } from './config.js';
+
+export class SimulationClient {
+  constructor(
+    private fetchFunction: typeof fetch,
+    private urls: Config['requestUrls']
+  ) {}
+
+  async getRawMapppings() {
+    const res = await this.fetchFunction(this.urls.mappings);
+    const data = (await res.json()) as unknown;
+    return data;
+  }
+
+  async getRawOdds() {
+    const res = await this.fetchFunction(this.urls.odds);
+    const data = (await res.json()) as unknown;
+    return data;
+  }
+}
