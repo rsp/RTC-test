bb9cbd857f7f0a8fd1ac19458404a15e39c74b91
Rafał Pocztarski <r.pocztarski@gmail.com>
2025-02-17 07:29:58 +0100

    Add configuration and defaults

---
+++ src/config.ts
@@ -0,0 +1,30 @@
+const DEFAULT_API_PORT = 3001;
+const DEFAULT_ROOT_PATH = '/client';
+const DEFAULT_UPDATE_INTERVAL_MS = 1000;
+const DEFAULT_STATE_URL = 'http://localhost:3000/api/state';
+const DEFAULT_MAPPINGS_URL = 'http://localhost:3000/api/mappings';
+
+export interface Config {
+  api: {
+    port: number;
+    root: string;
+  };
+  requestUrls: {
+    mappings: string;
+    state: string;
+  };
+  updateIntervalMs: number;
+}
+
+export const config: Config = {
+  api: {
+    port: Number(process.env.RTC_ADAPTER_API_PORT) || DEFAULT_API_PORT,
+    root: process.env.RTC_ADAPTER_ROOT_PATH ?? DEFAULT_ROOT_PATH,
+  },
+  requestUrls: {
+    mappings: process.env.RTC_ADAPTER_MAPPINGS_URL ?? DEFAULT_MAPPINGS_URL,
+    state: process.env.RTC_ADAPTER_STATE_URL ?? DEFAULT_STATE_URL,
+  },
+  updateIntervalMs:
+    Number(process.env.RTC_ADAPTER_UPDATE_INTERVAL_MS) || DEFAULT_UPDATE_INTERVAL_MS,
+};
